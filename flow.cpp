dcd6e64b152d74e3d11597fbccb779c29e1fe19d
- <rayhanjilani64@gmail.com>
2025-03-10 13:48:39 +0700

    nambah2 fitur search & sort

--- flow.cpp
+++ flow.cpp
@@ -26,8 +26,20 @@ struct akun_penyewa{
 
 // KUMPULAN DATA (UNIT, AKUN, ADMIN, ETC) MASUKIN SINI
 // data unit
-unit data_unit[301]; 
-int jumlahUnit = 3;
+unit data_unit[301]={
+
+    {"1", "Apartemen A", "Studio", "Jakarta", "AC, TV, Kulkas, Kamar Mandi", 1000000, true},
+    {"2", "Apartemen B", "Studio", "Jakarta", "Kolam Renang, Gym", 5000000, true},
+    {"3", "Apartemen C", "2BR", "Bandung", "Gym, Tempat Yoga", 7000000, false},
+    {"4", "Apartemen D", "1BR", "Surabaya", "Bioskop Pribadi", 6000000, true},
+    {"5", "Apartemen E", "3BR", "Jakarta", "Kolam Renang", 9000000, false},
+    {"6", "Apartemen F", "2BR", "Sleman", "Jasa Antar MBG, Akun Kaskus Premium", 1000000, false},
+    {"7", "Apartemen G", "Studio", "Surabaya", "-", 2000000, true},
+    {"8", "Apartemen H", "3BR", "Sleman", "-", 1500000, true},
+    {"9", "Apartemen I", "Loft", "Surabaya", "WiFi 6G (illegal), Jasa Antar Makanan", 2000000, true},
+    
+    }; 
+int jumlahUnit = 9;
 
 
 // data akun penyewa
@@ -52,10 +64,19 @@ void tambahDataApartemen();
 void hapusDataApartemen(); 
 void ubahStatusApartemen();
 
+void cariDataApartemen();
+void cariIDapartemen();
+void cariTipeApartemen();
+void cariHargaApartemen();
+void cariRangeHargaApartemen();
+void cariKetersediaanApartemen();
+void urutKecilBesar(unit data_unit[],int jmldata);
+void urutBesarKecil(unit data_unit[], int jmldata);
 
 int main() {
-    loginMenu();
-    return 0;
+    // loginMenu();
+    // return 0;
+    tampilkanDataApartemen();
 }
 
 //Menu Awal
@@ -285,7 +306,28 @@ void ubahStatusApartemen() {
 }
 
 void tampilkanDataApartemen() {
-    
+    tampilkanSemuaDataApartemen();
+
+    int menu;
+    cout << "\n1. Cari Data\n";
+    cout << "2. Urutkan Harga (rendah ke tinggi)\n";
+    cout << "3. Urutkan Harga (tinggi ke rendah)\n";
+    cout << "Pilih : "; cin >> menu;
+
+    switch (menu)
+    {
+    case 1:
+        cariDataApartemen();
+        break;
+    case 2:
+        urutKecilBesar(data_unit,jumlahUnit);
+        break;
+    case 3:
+        urutBesarKecil(data_unit,jumlahUnit);
+        break;
+    default:
+        break;
+    }
 }
 
 void sewaUnit() {
@@ -311,22 +353,243 @@ void hitungTotalPendapatan() {
 }
 
 void tampilkanSemuaDataApartemen(){
-    int IDlenght=6, namaLenght=20, tipeLenght=10, hargaLenght=20, tersediaLenght=13;
-    for (int i=1; i <=jumlahUnit; i++) {
-        if (i==1){
-            cout <<"==================================================================================" << endl;
-            cout << left << "|| " << setw(IDlenght) << "ID" << "| " << setw(namaLenght) << "nama" << "| " << setw(tipeLenght) << "tipe"  << "| " << setw(hargaLenght) << "harga/bulan"  << "| " << setw(tersediaLenght) << "tersedia"  << "||" << endl;
-            cout <<"==================================================================================" << endl;
-            cout << left << "|| " << setw(IDlenght) << data_unit[i].ID << "| " << setw(namaLenght) << data_unit[i].nama << "| " << setw(tipeLenght) << data_unit[i].tipe  << "| " << setw(hargaLenght) << data_unit[i].hargaPerBulan  << "| " << setw(tersediaLenght) << data_unit[i].statusTersedia  << "||" << endl;
-        } else if (i==jumlahUnit) {
-            cout << left << "|| " << setw(IDlenght) << data_unit[i].ID << "| " << setw(namaLenght) << data_unit[i].nama << "| " << setw(tipeLenght) << data_unit[i].tipe  << "| " << setw(hargaLenght) << data_unit[i].hargaPerBulan  << "| " << setw(tersediaLenght) << data_unit[i].statusTersedia  << "||" << endl;
-            cout <<"==================================================================================" << endl;
+    int IDlength = 6, namaLength = 21, tipeLength = 10, hargaLength = 20, tersediaLength = 13;
+    int totalLength = IDlength + namaLength + tipeLength + hargaLength + tersediaLength + 13;
 
+    cout << setfill('=') << setw(totalLength) << "" << endl;
+    cout << setfill(' ') << left << "|| " << setw(IDlength) << "ID" << "| " << setw(namaLength) << "nama" << "| " << setw(tipeLength) << "tipe"  << "| " << setw(hargaLength) << "harga/bulan"  << "| " << setw(tersediaLength) << "tersedia"  << "||" << endl;
+    cout << setfill('-') << setw(totalLength) << "" << endl;;
+    for (int i=0; i < jumlahUnit; i++) {
+        cout << setfill(' ') << left << "|| " << setw(IDlength) << data_unit[i].ID << "| " << setw(namaLength) << data_unit[i].nama << "| " << setw(tipeLength) << data_unit[i].tipe  << "| " << setw(hargaLength) << data_unit[i].hargaPerBulan  << "| " << setw(tersediaLength) << data_unit[i].statusTersedia  << "||" << endl;
+    }
+    cout << setfill('=') << setw(totalLength) << "" << endl;
+}
+
+void cariDataApartemen(){
+
+    int menu;
+    cout << "\nCari Data berdasarkan :\n";
+    cout << "1. ID\n";
+    cout << "2. Tipe\n";
+    cout << "3. Harga\n";
+    cout << "4. Range Harga\n";
+    cout << "5. Ketersediaan\n";
+    cout << "Pilih : "; cin >> menu;
+
+    switch (menu)
+    {
+    case 1:
+        cariIDapartemen();
+        break;
+    case 2:
+        cariTipeApartemen();
+        break;
+    case 3:
+        cariHargaApartemen();
+        break;
+    case 4:
+        cariRangeHargaApartemen();
+        break;
+    case 5:
+        cariKetersediaanApartemen();
+        break;
+    default:
+        break;
+    }
+
+
+}
+
+void cariIDapartemen(){
+    string cari;
+    cout << "Masukkan ID Apartemen yang ingin dicari : "; cin >> cari;
+
+    //asumsi ID urut
+
+    int bwh = 0, atas = jumlahUnit;
+    bool ketemu = false;
+
+    while (bwh <= atas)
+    {
+        int tgh = (bwh + atas) / 2;
+        if (data_unit[tgh].ID == cari)
+        {
+            cout << "\nNama : " << data_unit[tgh].nama << endl;
+            cout << "Tipe : " << data_unit[tgh].tipe << endl;
+            cout << "Lokasi : " << data_unit[tgh].lokasi << endl;
+            cout << "Fasilitas : " << data_unit[tgh].fasilitas << endl;
+            cout << "Harga : " << data_unit[tgh].hargaPerBulan << " / bulan\n";
+            cout << "Ketersediaan : " << data_unit[tgh].statusTersedia << endl;
+            ketemu = true;
+            break;
         } 
-        else {
-            cout << left << "|| " << setw(IDlenght) << data_unit[i].ID << "| " << setw(namaLenght) << data_unit[i].nama << "| " << setw(tipeLenght) << data_unit[i].tipe  << "| " << setw(hargaLenght) << data_unit[i].hargaPerBulan  << "| " << setw(tersediaLenght) << data_unit[i].statusTersedia  << "||" << endl;
+        else if (data_unit[tgh].ID > cari)
+        {
+            atas = tgh - 1;
+        }
+        else if (data_unit[tgh].ID < cari)
+        {
+            bwh = tgh + 1;
         }
     }
+
+    if (ketemu == false)
+    {
+        cout << "\nData apartemen dengan ID " << cari << " tidak ditemukan.";
+    }
+}
+
+void cariTipeApartemen(){
+    string cari;
+    cout << "Masukkan tipe apartemen : "; cin.ignore(); getline(cin,cari);
+
+    bool ketemu = false;
+
+    for (int i = 0; i < jumlahUnit; i++)
+    {
+        if (data_unit[i].tipe == cari)
+        {
+            cout << "\nID : " << data_unit[i].ID << endl;
+            cout << "Nama : " << data_unit[i].nama << endl;
+            cout << "Lokasi : " << data_unit[i].lokasi << endl;
+            cout << "Fasilitas : " << data_unit[i].fasilitas << endl;
+            cout << "Harga : " << data_unit[i].hargaPerBulan << " / bulan\n";
+            cout << "Ketersediaan : " << data_unit[i].statusTersedia << endl;
+            ketemu = true;
+        }
+    }
+
+    if (ketemu == false)
+    {
+        cout << "\nTipe apartemen " << cari << " tidak ditemukan.";
+    }
+    
+    
+}
+
+void cariHargaApartemen(){
+    int cari;
+    cout << "Masukkan harga apartemen : "; cin >> cari;
+
+    bool ketemu = false;
+
+    for (int i = 0; i < jumlahUnit; i++)
+    {
+        if (data_unit[i].hargaPerBulan == cari)
+        {
+            cout << "\nID : " << data_unit[i].ID << endl;
+            cout << "Nama : " << data_unit[i].nama << endl;
+            cout << "Tipe : " << data_unit[i].tipe << endl;
+            cout << "Lokasi : " << data_unit[i].lokasi << endl;
+            cout << "Fasilitas : " << data_unit[i].fasilitas << endl;
+            cout << "Ketersediaan : " << data_unit[i].statusTersedia << endl;
+            ketemu = true;
+        }
+    }
+
+    if (ketemu == false)
+    {
+        cout << "\nApartemen dengan harga " << cari << " tidak ditemukan.";
+    }
+    
+}
+
+void cariRangeHargaApartemen(){
+    int caribwh, cariatas;
+    cout << "Masukkan batas bawah harga apartemen : "; cin >> caribwh;
+    cout << "Masukkan batas atas harga apartemen  : "; cin >> cariatas;
+
+    if (caribwh <= cariatas){
+
+    bool ketemu = false;
+
+    for (int i = 0; i < jumlahUnit; i++)
+    {
+        if (data_unit[i].hargaPerBulan >= caribwh && data_unit[i].hargaPerBulan <= cariatas)
+        {
+            cout << "\nID : " << data_unit[i].ID << endl;
+            cout << "Nama : " << data_unit[i].nama << endl;
+            cout << "Tipe : " << data_unit[i].tipe << endl;
+            cout << "Lokasi : " << data_unit[i].lokasi << endl;
+            cout << "Fasilitas : " << data_unit[i].fasilitas << endl;
+            cout << "Harga : " << data_unit[i].hargaPerBulan << " / bulan\n";
+            cout << "Ketersediaan : " << data_unit[i].statusTersedia << endl;
+            ketemu = true;
+        }
+    }
+
+    if (ketemu == false)
+    {
+        cout << "\nApartemen dengan range harga " << caribwh << " - " << cariatas << " tidak ditemukan.";
+    }
+
+    } else { 
+    cout << "\nRange harga invalid.";
+    }
+}
+
+void cariKetersediaanApartemen(){
+    int cari;
+
+    cout << "1. Cari apartemen yang diisi\n";
+    cout << "0. Cari apartemen yang kosong\n";
+    cout << "Pilih : "; cin >> cari;
+
+    bool ketemu;
+
+    if (cari == 1 || cari == 0) {
+        for (int i = 0; i < jumlahUnit; i++)
+    {
+        if (data_unit[i].statusTersedia == cari) 
+        {
+            cout << "\nID : " << data_unit[i].ID << endl;
+            cout << "Nama : " << data_unit[i].nama << endl;
+            cout << "Tipe : " << data_unit[i].tipe << endl;
+            cout << "Lokasi : " << data_unit[i].lokasi << endl;
+            cout << "Fasilitas : " << data_unit[i].fasilitas << endl;
+            cout << "Harga : " << data_unit[i].hargaPerBulan << " / bulan\n";
+            cout << "Ketersediaan : " << data_unit[i].statusTersedia << endl;
+            ketemu = true;
+        }
+    }
+    } 
+    else {
+        cout << "\nPilihan Invalid\n";
+    }
+}
+
+void urutKecilBesar(unit data_unit[], int jmldata){
+    for (int i = 0; i < jmldata; i++)
+    {
+        for (int j = i + 1; j < jmldata; j++)
+        {
+            if (data_unit[j].hargaPerBulan < data_unit[i].hargaPerBulan)
+            {
+               int temp = data_unit[i].hargaPerBulan;
+               data_unit[i].hargaPerBulan = data_unit[j].hargaPerBulan;
+               data_unit[j].hargaPerBulan = temp;
+            }   
+        }
+    }
+    
+    tampilkanSemuaDataApartemen(); 
+}
+
+void urutBesarKecil(unit data_unit[], int jmldata){
+    for (int i = 0; i < jmldata; i++)
+    {
+        for (int j = i + 1; j < jmldata; j++)
+        {
+            if (data_unit[j].hargaPerBulan > data_unit[i].hargaPerBulan)
+            {
+               int temp = data_unit[i].hargaPerBulan;
+               data_unit[i].hargaPerBulan = data_unit[j].hargaPerBulan;
+               data_unit[j].hargaPerBulan = temp;
+            }   
+        }
+    }
+    
+    tampilkanSemuaDataApartemen();
 }
 
 bool cariUnit(int index, string& pilihID_unit) {
