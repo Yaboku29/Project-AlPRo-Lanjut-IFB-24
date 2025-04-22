86fb17608ff178b6d89a5e0c89de68126f1e0bc8
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2025-04-22 10:41:20 +0700

    tambah file cari_data

---
+++ cari_data.cpp
@@ -0,0 +1,26 @@
+#include "cari_data.hpp"
+
+void cariDataApartemen(){
+
+}
+void cariIDapartemen(){
+
+}
+void cariTipeApartemen(){
+
+}
+void cariHargaApartemen(){
+
+}
+void cariRangeHargaApartemen(){
+
+}
+void cariKetersediaanApartemen(){
+
+}
+void urutKecilBesar(unit data_unit[],int jmldata){
+
+}
+void urutBesarKecil(unit data_unit[], int jmldata){
+
+}
---
+++ cari_data.hpp
@@ -0,0 +1,16 @@
+#ifndef CARI_DATA_HPP
+#define CARI_DATA_HPP
+
+#include <iostream>
+#include "Data_Apartment.hpp"
+
+void cariDataApartemen();
+void cariIDapartemen();
+void cariTipeApartemen();
+void cariHargaApartemen();
+void cariRangeHargaApartemen();
+void cariKetersediaanApartemen();
+void urutKecilBesar(unit data_unit[],int jmldata);
+void urutBesarKecil(unit data_unit[], int jmldata);
+
+#endif
