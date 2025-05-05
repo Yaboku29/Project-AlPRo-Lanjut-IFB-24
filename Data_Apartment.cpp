cd65e06f49f0185bbfe340e15e70f98ec8f17592
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2025-05-06 02:46:30 +0700

    Hapus caridata di Data_Aprtment

--- Data_Apartment.cpp
+++ Data_Apartment.cpp
@@ -26,27 +26,3 @@ void ubahStatusApartemen(){
     clearScreen();
 }
 //===================================
-void cariDataApartemen(){
-    clearScreen();
-}
-void cariIDapartemen(){
-    clearScreen();
-}
-void cariTipeApartemen(){
-    clearScreen();
-}
-void cariHargaApartemen(){
-    clearScreen();
-}
-void cariRangeHargaApartemen(){
-    clearScreen();
-}
-void cariKetersediaanApartemen(){
-    clearScreen();
-}
-void urutKecilBesar(unit data_unit[],int jmldata){
-    clearScreen();
-}
-void urutBesarKecil(unit data_unit[], int jmldata){
-    clearScreen();
-}
--- Data_Apartment.hpp
+++ Data_Apartment.hpp
@@ -25,14 +25,5 @@ void tambahDataApartemen();
 void hapusDataApartemen(); 
 void ubahStatusApartemen();
 
-void cariDataApartemen();
-void cariIDapartemen();
-void cariTipeApartemen();
-void cariHargaApartemen();
-void cariRangeHargaApartemen();
-void cariKetersediaanApartemen();
-void urutKecilBesar(unit data_unit[],int jmldata);
-void urutBesarKecil(unit data_unit[], int jmldata);
-
 
 #endif
